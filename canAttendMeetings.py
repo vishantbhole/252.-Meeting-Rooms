b6b8f1b9624a7460d58636a364e05f076fd3200b
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-24 20:05:57 -0600

    Update canAttendMeetings.py

--- canAttendMeetings.py
+++ canAttendMeetings.py
@@ -4,3 +4,10 @@ class Interval(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
+
+
+from typing import List
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
