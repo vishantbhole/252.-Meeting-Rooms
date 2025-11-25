e45e1920b4c78b8c1cba3a620e657e135fd57633
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-24 20:06:46 -0600

    Update canAttendMeetings.py

--- canAttendMeetings.py
+++ canAttendMeetings.py
@@ -11,3 +11,9 @@ from typing import List
 
 class Solution:
     def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        
+        intervals.sort(key = lambda i : i.start)
+
+        for i in range(1, len(intervals)):
+            prev_it = intervals[i - 1]
+            curr_it = intervals[i]
